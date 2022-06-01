017329d8ef699b7c7b2578395ca535b4bf4549c3
wazmi01 <106710831+wazmi01@users.noreply.github.com>
2022-06-01 16:33:52 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 terraform {
   required_version = ">= 0.12.0"
 }
-
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
+}
 provider "aws" {
 }
 
